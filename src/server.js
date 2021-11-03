94f3fe8d8bca36628cbc2185dcf726dfbeeb9534
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-03 16:06:38 +0100

    setup

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+
+const server = express(); // const app = express();
+const port = 3001; //server to listen on the port, it is stored into a variable
+
+// server.use(loggerMiddleware)
+//cors and express are middlewares
+server.use(express.json()); //this has to be specified BEFORE the routes, otherwise the body will be undefined *** the same as app.use(express.json());
+server.use(cors()); //cors connects BE with FE *** the same as app.use(cors());
+
+// *********************** ROUTES ***************************
+// server.use("/cart", cartRouter);
+// server.use("/products", productsRouter);
+// server.use("/reviews", reviewsRouter);
+// server.use("/users", usersRouter);
+// server.use("/categories", categoriesRouter);
+
+
+server.listen(port, async () => {
+    try {
+    //   await syncSequelize();
+      console.log(`❤ Server is running on port ${port}`);
+    } catch (error) {
+      console.log(error);
+    }
+  });
