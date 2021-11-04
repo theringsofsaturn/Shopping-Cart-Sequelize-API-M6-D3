08addb33364681f23f9cc557291c3ef1c4d90448
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-04 04:39:10 +0100

    user find all

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -18,5 +18,18 @@ router
   }
 });
 
+// SELECT
+router
+.route("/")
+.get(async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 
 export default router;
